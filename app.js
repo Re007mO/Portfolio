fb4d4a7c8ad97ae99a122357759f87d4d5feca7b
Reem Mohamed Salah <remosmosalah@gmail.com>
2024-08-28 22:14:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const whatsapp = document.getElementById("whatsapp")
+whatsapp.addEventListener("click",function(){
+    alert("This is my Contact number: 01117285571 ")
+})
+
+
+const telegram = document.getElementById("telegram")
+telegram.addEventListener("click",function(){
+    alert("This is my User name on Telegram : @reemosalah ,search it!!")
+})
+
+
+const gmail = document.getElementById("gmail")
+gmail.addEventListener("click",function(){
+    alert("This is my Gmail: remosmosalah@gmail.com ,search it!!")
+})
