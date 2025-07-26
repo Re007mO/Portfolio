dacc7150c0ca77ddd9587dc9e71c5d2e09006193
Reem Mohamed Salah <remosmosalah@gmail.com>
2025-07-26 16:28:47 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 
+
 const telegram = document.getElementById("telegram")
 telegram.addEventListener("click",function(){
     alert("This is my User name on Telegram : @reemosalah ,search it!!")
 })
 
 
-const gmail = document.getElementById("gmail")
-gmail.addEventListener("click",function(){
-    alert("This is my Gmail: remosmosalah@gmail.com ,search it!!")
-})
+const hamburger = document.getElementById('hamburger');
+const navUl = document.getElementById('nav-ul');
+
+hamburger.addEventListener('click', () => {
+  navUl.classList.toggle('active');
+});
